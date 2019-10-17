851ab92ce708aa04e1bae8642e77d1ba2bfc06e8
RelNet <jyubbsteam@outlook.com>
2019-10-17 20:12:23 +0800

    ...

--- OJ/src/CompileFile/CompileGPP.java
+++ OJ/src/CompileFile/CompileGPP.java
@@ -1,4 +1,39 @@
 package CompileFile;
 
-public class CompileGPP {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CompileGPP extends CompileMain {
+    public CompileGPP(File inFile, String inSubmitID) {
+        super(inFile, inSubmitID);
+    }
+
+    @Override
+    public void compileIt() {
+        Process order;
+        try {
+            order = Runtime.getRuntime().exec("g++ -o " + INITIAL_FILE_ADDRESS + "/" + submitID + "/t" + submitID + sourceCodeFile.toString());
+            BufferedReader inError = new BufferedReader(new InputStreamReader(order.getInputStream()));
+            String str;
+            while ((str = inError.readLine()) != null) {
+                errorCodes.add(str);
+            }
+            inError.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    // 检查errorCodes，如果出现字串"error"则说明编译出错
+    public boolean hasCompileError() {
+        for (var errorCode : errorCodes) {
+            if (errorCode.contains("error")) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- OJ/src/CompileFile/Test/TestCompileGPP.java
+++ OJ/src/CompileFile/Test/TestCompileGPP.java
@@ -1,4 +1,22 @@
 package CompileFile.Test;
 
+import CompileFile.CompileGPP;
+import CompileFile.CompileMain;
+
+import java.io.File;
+import java.util.List;
+
 public class TestCompileGPP {
+    public static void main(String[] args) {
+        File f = new File("/home/h/Desktop/test.cpp");
+        CompileMain test = new CompileGPP(f, "1");
+
+        test.compileIt();
+        if (test.hasCompileError()) {
+            List<String> strings = test.getErrorCodes();
+            for (String i : strings) {
+                System.out.println(i);
+            }
+        }
+    }
 }
