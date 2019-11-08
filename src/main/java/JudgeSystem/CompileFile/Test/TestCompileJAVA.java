815d2033ec83d10db5c2eed3015104eec24042b3
黄子涵 <h@huangzihandeMacBook-Pro.local>
2019-11-08 12:17:48 +0800

    能够编译

--- src/main/java/JudgeSystem/CompileFile/Test/TestCompileJAVA.java
+++ src/main/java/JudgeSystem/CompileFile/Test/TestCompileJAVA.java
@@ -6,33 +6,4 @@ import JudgeSystem.CompileFile.CompileMain;
 import java.io.File;
 import java.util.List;
 
-public class TestCompileJAVA {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public static void main(String[] args) {
-=======
-//    public static void main(String[] args) {
->>>>>>> 6e9b4a50a74e5b7d464672ac8a59ffb6352bfaad
-=======
-//    public static void main(String[] args) {
->>>>>>> Backend-database
-//        File f = new File("/home/h/Desktop/testJ.java");
-//        CompileMain test = new CompileJAVA(f, "1");
-//
-//        test.compileIt();
-//        if (test.hasCompileError()) {
-//            List<String> strings = test.getErrorCodes();
-//            for (String i : strings) {
-//                System.out.println(i);
-//            }
-//        }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    }
-=======
-//    }
->>>>>>> 6e9b4a50a74e5b7d464672ac8a59ffb6352bfaad
-=======
-//    }
->>>>>>> Backend-database
-}
+
